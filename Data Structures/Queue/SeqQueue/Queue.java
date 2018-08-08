9e6886f4160c5be2ea467efd1882960298a43f17
lele <wanle@qq.com>
2018-08-08 09:16:43 +0800

    first commit

---
+++ Data Structures/Queue/SeqQueue/Queue.java
@@ -0,0 +1,13 @@
+package SeqQueue;
+
+public interface Queue {
+
+    public void append(Object obj) throws Exception;
+
+    public Object delete() throws Exception;
+
+    public Object getFront() throws Exception;
+
+    public boolean notEmpty();
+
+}
