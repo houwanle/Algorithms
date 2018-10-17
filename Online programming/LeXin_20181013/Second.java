02ca0ce31f10ee83e4033f68c88051033f40df7b
lele <wanle@qq.com>
2018-10-17 18:25:46 +0800

    modify

--- Online programming/LeXin_20181013/Second.java
+++ Online programming/LeXin_20181013/Second.java
@@ -5,8 +5,37 @@
 	（2）    探索迷宫的四个方向：RIGHT为向右，DOWN向下，LEFT向左，UP向上，输出从入口到出口的行走路径。
  * 
  */
+import java.text.ParseException;
 import java.util.Scanner;
 
-public class Second {  
-    
-}  
+public class Second {
+	static int res = 0;
+
+	public static void main(String[] args) throws ParseException {
+		Scanner in = new Scanner(System.in);
+		int row = in.nextInt();
+		int col = in.nextInt();
+		int a[][] = new int[row][col];
+		int b[][] = new int[row][col];
+		for (int i = 0; i < row; i++)
+			for (int j = 0; j < col; j++)
+				a[i][j] = in.nextInt();
+		dfs(a, 0, 0, row, col, b);
+		System.out.println(res);
+	}
+
+	public static void dfs(int a[][], int i, int j, int row, int col, int b[][]) {
+		if (i == row - 1 && j == col - 1) {
+			res++;
+			return;
+		}
+		if (i >= row || j >= col || i < 0 || j < 0 || a[i][j] == 1 || b[i][j] == 1)
+			return;
+		b[i][j] = 1;
+		dfs(a, i - 1, j, row, col, b);
+		dfs(a, i + 1, j, row, col, b);
+		dfs(a, i, j - 1, row, col, b);
+		dfs(a, i, j + 1, row, col, b);
+		b[i][j] = 0;
+	}
+}
